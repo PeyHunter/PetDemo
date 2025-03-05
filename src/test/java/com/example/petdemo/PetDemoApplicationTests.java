2d8b119feb2cf5a0020404d4ee9d347c88c27624
PeyHunter <Nando.goat@gmail.com>
2025-03-05 18:26:05 +0100

    Pet Demo

---
+++ src/test/java/com/example/petdemo/PetDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.petdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
